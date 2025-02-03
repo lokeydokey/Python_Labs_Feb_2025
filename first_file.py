9ab63818022bdc1b0fc3ae114872db5a74ebbad4
Lokesh Verma <lokesh.v.verma@oracle.com>
2025-02-03 22:52:54 +0000

    Added first.py

---
+++ first_file.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+# Author: Lverma
+#Version: 1.0
+#Description: This is our first script
+
+#name="Lokesh Verma"
+name = input("Enter Your Name : ")
+
+print( "My name is ",name)
+print( "My name is ", name)
+
+import random
+print("My lucky number is ", random.randint(1,50))
+
+import math
+print("cosine of 0.5 is", math.cos(0.5))

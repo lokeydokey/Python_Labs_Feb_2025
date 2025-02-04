3fc20b24270e14f8460812ff1fdc74f9893453d5
Lokesh Verma <lokesh.v.verma@oracle.com>
2025-02-04 18:54:20 +0000

    Added demo_platform.py

---
+++ demo_platform.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+# Author: Lverma
+# Description: This script will demo
+"""
+    Docstring:
+"""
+import os
+import sys
+if sys.platform == "win32":
+    home_dir = os.environ["HOMEPATH"]
+else:
+    home_dir = os.environ["HOME"]
+
+print(home_dir)
